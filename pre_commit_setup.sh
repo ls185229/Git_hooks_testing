be49fb2e4713a6d0ead954c3d4a8ce24b8875f19
ls185229 <ls185229@ncr.com>
2021-08-30 17:56:34 +0530

    NPP-123 : .pre-commit file updated

--- pre_commit_setup.sh
+++ pre_commit_setup.sh
@@ -8,8 +8,9 @@ pip install pre-commit
 echo "pre-commit package installed"
 pre-commit --version
 pre-commit install
-pre-commit autoupdate
+# pre-commit autoupdate 
+# if you execute the above command, .pre-commit-config.ymal file will be modified
+# since that file is being tracked remotely, it is not recommended to run often
 pre-commit install --install-hooks
 
 echo "Local pre-commit set up is completed"
-
